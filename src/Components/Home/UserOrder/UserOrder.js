f57506533ba9f31322cc351faa017e6ae041fd4e
unknown <you@example.com>
2021-11-21 21:43:59 +0600

    my Orders Added

--- src/Components/Home/UserOrder/UserOrder.js
+++ src/Components/Home/UserOrder/UserOrder.js
@@ -1,7 +1,12 @@
 import React, { useEffect, useState } from 'react';
-import { Button } from '@mui/material';
+import { Button, Grid, Container, Box } from '@mui/material';
 import useAuth from '../../../hooks/useAuth';
-
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import Header from './../../Shared/Header/Header';
 const UserOrder = () => {
     const [products, setProducts] = useState([]);
     const [isDelete, setIsDelete] = useState(null);
@@ -10,7 +15,7 @@ const UserOrder = () => {
         const url = `http://localhost:5000/userProduct?email=${user.email}`
         fetch(url)
             .then(res => res.json())
-            .then(result => console.log(result))
+            .then(result => setProducts(result))
 
 
     }, [isDelete]);
@@ -21,7 +26,7 @@ const UserOrder = () => {
     const handleDeleteProduct = (id) => {
         console.log(id);
 
-        fetch(`https://pure-garden-91979.herokuapp.com/deleteProduct/${id}`, {
+        fetch(`http://localhost:5000/deleteProduct/${id}`, {
             method: "DELETE",
             headers: { "Content-type": "application/json" },
         })
@@ -41,10 +46,67 @@ const UserOrder = () => {
 
 
     return (
-        <div>
-            <h2>This is User Order</h2>
-            {/* <Button onClick={() => handleDeleteProduct(product._id)} >This is Delet</Button> */}
-        </div>
+        <Box>
+            <Header />
+
+
+            <Container>
+                <Typography sx={{ textAlign: "center", mt: 4 }} variant="h4">My Orders</Typography>
+                <Grid container spacing={1}>
+
+                    {
+                        products.map(product =>
+                            <Grid md={6} xs={12}>
+                                <Card sx={{
+                                    maxWidth: '96% '
+                                }}>
+
+
+
+
+
+
+                                    < Grid container spacing={2} >
+                                        <Grid md={4} item xs={4}>
+                                            <CardMedia
+                                                component="img"
+                                                alt="green iguana"
+                                                height="100%"
+                                                image={product.img1}
+                                            />
+                                        </Grid>
+                                        <Grid md={8} item xs={8} >
+                                            <CardContent>
+                                                <Typography gutterBottom variant="h5" component="div">
+                                                    {product.name}
+                                                </Typography>
+                                                <Typography variant="body2" color="text.secondary">
+                                                    {product.discription1.slice(0, 100)}
+
+                                                </Typography>
+                                            </CardContent>
+                                            <CardActions>
+                                                <Button size="small">Share</Button>
+                                                <Button onClick={() => handleDeleteProduct(product._id)} size="small">Learn More</Button>
+                                            </CardActions>
+                                        </Grid>
+
+
+                                    </Grid>
+
+                                </Card>
+                            </Grid>
+                        )
+                    }
+
+
+
+                </Grid>
+
+            </Container >
+
+
+        </Box >
     );
 };
 
