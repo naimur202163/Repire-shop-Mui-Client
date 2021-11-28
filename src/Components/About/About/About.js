797ca964574726cd992b37578f1ca1cab4879efa
unknown <you@example.com>
2021-11-28 13:06:58 +0600

    card will added

--- src/Components/About/About/About.js
+++ src/Components/About/About/About.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Aboutbanner from '../About-banner/About-banner';
 import AboutMidBanner from './../AboutMidBanner/AboutMidBanner';
+import MeetOurTeam from './../MeetOuerTeam/MeetOurTeam';
 
 const About = () => {
     return (
         <div>
             <Aboutbanner></Aboutbanner>
             <AboutMidBanner></AboutMidBanner>
+            <MeetOurTeam></MeetOurTeam>
         </div>
     );
 };
--- src/Components/About/MeetOuerTeam/MeetOurTeam.js
+++ src/Components/About/MeetOuerTeam/MeetOurTeam.js
@@ -1,10 +1,40 @@
 import React from 'react';
-import { Box } from '@mui/material';
-import './MeetOurTeam.css'
+import { Box, Container, Typography } from '@mui/material';
+import './MeetOurTeam.css';
+
+
+const Teams = [
+    {
+        name: "Ben Stcoks",
+        test: 'Engineer',
+        img: "https://santhemes.com/tidytheme/fizxila/images/team1.jpg"
+    },
+    {
+        name: "Adam Crew",
+        test: 'Engineer',
+        img: "https://santhemes.com/tidytheme/fizxila/images/team2.jpg"
+    },
+    {
+        name: "Moris Jon",
+        test: 'Manager',
+        img: "https://santhemes.com/tidytheme/fizxila/images/team3.jpg"
+    },
+]
 const MeetOurTeam = () => {
     return (
         <Box className="bg-images">
-
+            <Container>
+                <Box>
+                    <Box>
+                        <Typography sx={{ textAlign: 'center', my: 10, mt: 2 }} variant="h4">Meet Our Team</Typography>
+                        <Typography sx={{ textAlign: 'center', mt: 2 }} variant="subtitle1">Service features tended no do thoughts me on dissuade scarcely
+                            <br />
+                            own are pretty spring suffer
+                            <br />old denote his proposal speedily amr striking am now.</Typography>
+                    </Box>
+                    <Box></Box>
+                </Box>
+            </Container>
         </Box>
     );
 };
